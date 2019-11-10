dfbd4dd2e3367c375e19accc1f76963f49f5e11f
MrMarosis <bartlomiej.t.stachura@gmail.com>
2019-11-10 14:41:57 +0100

    Adjust apollo configuration, update package json

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     client: {
         addTypename: true,
+        url: 'http://localhost:5001/graphql',
         includes: ['./src/**'],
         service: {
             name: 'abawesome-backend',
-            localSchemaFile: './schema.json',
+            // localSchemaFile: './schema.json',
         },
     },
 };
